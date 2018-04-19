9b9b6692e65ad65a66a64a7bdf189292728a2817
Kenny Davis <kjdavis2423@gmail.com>
2018-04-19 22:21:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   
   return object
 }
+
+function deleteFromObjectByKey(object, key){
+  delete object.key
+  
+  return object
+}
